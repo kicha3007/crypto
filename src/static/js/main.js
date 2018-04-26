99018caafd7bbfd9386830b822f0f1cb8d425515
Kicha3007 <kicha3007@gmail.com>
2018-04-26 13:13:44 +0500

    add fix sidebar

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,19 +1,47 @@
+
+/* @Note: not sure e.pageX will work in IE8 */
+(function(window){
+
+    /* A full compatability script from MDN: */
+    var supportPageOffset = window.pageXOffset !== undefined;
+    var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+
+    /* Set up some variables  */
+    var demoItem2 = document.getElementById("demoItem2");
+    var demoItem3 = document.getElementById("demoItem3");
+    /* Add an event to the window.onscroll event */
+    window.addEventListener("scroll", function(e) {
+
+        /* A full compatability script from MDN for gathering the x and y values of scroll: */
+        var x = supportPageOffset ? window.pageXOffset : isCSS1Compat ? document.documentElement.scrollLeft : document.body.scrollLeft;
+        var y = supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+
+        demoItem2.style.left = -x + 50 + "px";
+        demoItem3.style.top = -y + 50 + "px";
+    });
+
+})(window);
+
+
+
+
+
 $(function () {
 
     /* ------------------- google charts ------------------- */
 
-    google.charts.load("current", {packages:["corechart"]});
+    google.charts.load("current", {packages: ["corechart"]});
     google.charts.setOnLoadCallback(drawChart);
 
     function drawChart() {
         var data = google.visualization.arrayToDataTable([
             ['Task', 'Hours per Day'],
-            ['Miners', ""     ],
-            ['Developers',      2],
-            ['Presale',  2],
+            ['Miners', ""],
+            ['Developers', 2],
+            ['Presale', 2],
             ['Bounty campaign', 2],
-            ['Crowdsale',    7],
-            ['Partnership',    7]
+            ['Crowdsale', 7],
+            ['Partnership', 7]
         ]);
 
         var options = {
@@ -41,7 +69,7 @@ $(function () {
         var showMoreButtonCloseTextToButton = $this.text();
 
         $this.text(showMoreButtonCloseText);
-        $this.data("showMoreButton", showMoreButtonCloseTextToButton );
+        $this.data("showMoreButton", showMoreButtonCloseTextToButton);
 
         var showMoreButtonValue = $this.attr("href");
         var showMoreText = $(showMoreButtonValue);
@@ -114,7 +142,7 @@ $(function () {
         mainSidebar = $("[data-main-sidebar]");
 
     var windowSizeMax = window.matchMedia('all and (max-width: 576px)');
-   var windowSizeMaxAll = window.matchMedia('(min-width: 1200px)');
+    var windowSizeMaxAll = window.matchMedia('(min-width: 1200px)');
 
     $(document).on("scroll", function () {
         var documentScroll = $(this).scrollTop();
@@ -149,12 +177,13 @@ $(function () {
 
     /* ------------------- hide-scroll ------------------- */
 
-    var parentScrollHide = document.querySelector('[data-scroll-hide]');
-    var childScrollHide = document.querySelector('.it-main-nav-2__list');
-    childP = child.offsetHeight - child.clientHeight;
-    parent.style.height = parent.clientHeight - childP + "px";
+    var parent = document.querySelector('[data-scroll-hide]');
+    var child = document.querySelector('.it-main-nav-2__list');
+    var childP = + child.offsetHeight -  child.clientHeight;
 
-
+    if (childP) {
+         parent.style.height = parent.clientHeight - childP + "px";
+    }
 
     /* ****************************** dropdown-menu ****************************** */
 
